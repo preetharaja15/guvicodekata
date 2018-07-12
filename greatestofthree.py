3dc87b39bceb489501e7832a47c7b4279e1d2bbb
preetharaja15 <39819679+preetharaja15@users.noreply.github.com>
2018-07-12 14:34:27 +0530

    Update greatestofthree.py

--- greatestofthree.py
+++ greatestofthree.py
@@ -1,10 +1,14 @@
-a=input()
-b=input()
-c=input()
+aa=input()
+aa=aa.split()
+a=int(aa[0])
+b=int(aa[1])
+c=int(aa[2])
 if a>b:
 	if a>c:
-		print a
+		print (a)
+
 elif b>c:
-	print b
+	print (b)
 else:
-	print c
+	print (c)
+
