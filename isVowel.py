43095ba43a0122a8f44b7e0f4446cffd8415f857
preetharaja15 <39819679+preetharaja15@users.noreply.github.com>
2018-07-12 13:05:48 +0530

    Update isVowel.py

--- isVowel.py
+++ isVowel.py
@@ -1,10 +1,17 @@
 a=input()
 l=['a','e','i','o','u']
 n=len(a)
-if n>1:
-	print("Invalid")
-else:
-	if a in l:
-		print("Vowel")
+if a.isalpha():
+	if n>1:
+		print("Invalid")
 	else:
-		print("Consonant")
+		if a in l:
+			print("Vowel")
+		else:
+			print("Consonant")
+
+
+else:
+	print("invalid")	
+	
+	
